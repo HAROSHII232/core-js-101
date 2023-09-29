5404b2ac244f7696086e9ca3e3e1c4f4747fbda3
Igor Voitov <igorgrime@gmail.com>
2023-09-29 18:31:37 +0300

    feat: done loop tasks 8/17

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -220,8 +220,36 @@ function findFirstSingleChar(str) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  let result = '';
+
+  if (isStartIncluded) {
+    result += '[';
+  } else {
+    result += '(';
+  }
+
+  if (a > b) {
+    result += b;
+  } else {
+    result += a;
+  }
+
+  result += ', ';
+
+  if (b > a) {
+    result += b;
+  } else {
+    result += a;
+  }
+
+  if (isEndIncluded) {
+    result += ']';
+  } else {
+    result += ')';
+  }
+
+  return result;
 }
 
 
